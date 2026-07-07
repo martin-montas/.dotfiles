7c92a1037139dfcf3010a0f0eb5e520eb9308a92
martin-montas <martinmontas1@gmail.com>
2026-07-06 20:37:46 -0400

    new commits

--- .config/nvim/lua/martin-montas/core/options.lua
+++ .config/nvim/lua/martin-montas/core/options.lua
@@ -6,7 +6,7 @@ vim.api.nvim_command("set jumpoptions+=view")
 vim.api.nvim_command("set nowrap")
 
 vim.api.nvim_command("filetype plugin indent on")
-o.termguicolors = false
+o.termguicolors = true
 g.background = "dark"
 vim.api.nvim_command("syntax on")
 -- Decrease update time
